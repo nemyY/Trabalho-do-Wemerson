cee32168c48cff4c6af0e5de468d7493df93fd68
nemy <asmodeus0902@gmail.com>
2024-08-01 19:39:53 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("side-bar").style.width = "250px";
+  document.getElementById("hamburger-menu-container").style.marginLeft =
+    "250px";
+}
+
+function closeNav() {
+  document.getElementById("side-bar").style.width = "0";
+  document.getElementById("hamburger-menu-container").style.marginLeft = "0";
+}
